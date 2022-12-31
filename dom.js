26775fd9887276ccaf014851bd63c722e91c7b3a
Alejo Arenas <112776973+alearenass090@users.noreply.github.com>
2022-12-30 23:45:51 -0300

    Delete dom.js

--- dom.js
+++
@@ -1,6 +0,0 @@
-console.log(document.title);
-document.title = 'Prueba de titulo';
-console.log(document.title);
-console.log(document.body);
-var h1 = document.getElementsByTagName('h1');
-console.log(h1);
